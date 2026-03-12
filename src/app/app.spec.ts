7d96b4f8fcda7851a8e884c42c2cd6416d00fe81
Marc LEAUMENT <marc.leaument@atos.net>
2026-03-12 15:27:54 +0100

    Formation - Jour 2 - Partie 2

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,12 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { App } from './app';
-import { By } from '@angular/platform-browser';
-import { ProductCard } from './product/product-card';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { App } from './app';
+import { ProductCard } from './product/product-card';
+import { Product } from './product/product';
 
-describe('App', () => {
-  let component: App;
+describe('App (second approach - allowing unknown HTML elements)', () => {
   let fixture: ComponentFixture<App>;
+  let component: App;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -24,6 +25,10 @@ describe('App', () => {
     component = fixture.componentInstance;
   });
 
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
   it('should display the products', () => {
     fixture.detectChanges();
     const productDebugElements = fixture.debugElement.queryAll(By.css('app-product-card'));
@@ -31,14 +36,14 @@ describe('App', () => {
     expect(productDebugElements).toHaveLength(4);
 
     productDebugElements.forEach((productDebugElement, index) => {
-      expect(productDebugElement.properties['product']).toBe(component.products[index]);
+      expect(productDebugElement.properties['product']).toBe(component.products()[index]);
     });
   });
 
   // ----- DOM Testing (the hard way) -----
   it('should update the total when a product emits the "addToBasket" event', () => {
     // Given
-    component.total = 99;
+    component.total.set(99);
     fixture.detectChanges();
 
     const header = (fixture.nativeElement as HTMLElement).querySelector('header');
@@ -46,22 +51,70 @@ describe('App', () => {
 
     // When
     const productDebugElements = fixture.debugElement.queryAll(By.css('app-product-card'));
-    productDebugElements[1].triggerEventHandler('addToBasket', component.products[1]);
+    productDebugElements[1].triggerEventHandler('addToBasket', component.products()[1]);
     fixture.detectChanges();
 
     // Then
-    expect(header?.textContent).toContain(99 + component.products[1].price);
+    expect(header?.textContent).toContain(99 + component.products()[1].price);
   });
 
   // ----- Class Testing (the easy way) -----
   it('should update the total when "updateTotal" class method is called', () => {
     // Given
-    component.total = 99;
+    component.total.set(99);
 
     // When
-    component.updateTotal(component.products[1]);
+    component.updateTotal(component.products()[1]);
 
     // Then
-    expect(component.total).toBe(99 + component.products[1].price);
+    expect(component.total).toBe(99 + component.products()[1].price);
+  });
+
+
+  it('should decrease the stock of the product added to the basket', () => {
+    const product = component.products()[0];
+    // Given
+
+    expect(product.stock).toBe(2);
+
+    // When
+    const productDebugElement = fixture.debugElement.query(By.css('app-product-card'));
+    productDebugElement.triggerEventHandler('updateTotal', product);
+
+    // Then
+    expect(product.stock).toBe(1);
+  });
+
+  it('should not display products whose stock is empty', () => {
+    // Given
+    let productDebugElements = fixture.debugElement.queryAll(By.css('app-product-card'));
+    expect(productDebugElements).toHaveLength(4);
+
+    // When
+    component.products()[0].stock = 0;
+    component.products()[1].stock = 0;
+    fixture.detectChanges();
+
+    // Then
+    productDebugElements = fixture.debugElement.queryAll(By.css('app-product-card'));
+    expect(productDebugElements).toHaveLength(2);
+    expect(productDebugElements[0].properties['product']).toBe(component.products()[2]);
+    expect(productDebugElements[1].properties['product']).toBe(component.products()[3]);
+  });
+
+  it.only('should display the message "Désolé, notre stock est vide !" when the stock is completely empty', () => {
+    // Given
+    let element: HTMLElement | null = fixture.nativeElement.querySelector('.text-secondary');
+    expect(element).toBeNull();
+
+    // When
+    component.products.set([{ stock: 0 } as Product]);
+    fixture.detectChanges();
+
+    // Then
+    element = fixture.nativeElement.querySelector('.text-secondary');
+
+    expect(element).not.toBeNull();
+    expect(element?.textContent).toContain('Désolé, notre stock est vide !');
   });
 });
